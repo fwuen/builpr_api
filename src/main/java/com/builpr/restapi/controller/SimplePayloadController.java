44e1f733bece13b7c23145319828737def55bb19
Dominic Fuchs <syntarex@gmail.com>
2017-04-13 16:39:40 +0200

    Ergänzung Controller.

--- src/main/java/com/builpr/restapi/controller/SimplePayloadController.java
+++ src/main/java/com/builpr/restapi/controller/SimplePayloadController.java
@@ -2,13 +2,14 @@ package com.builpr.restapi.controller;
 
 import com.builpr.restapi.model.SimplePayload;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimplePayloadController {
 
-    @RequestMapping("/simplepayload")
+    @RequestMapping(value = "/simplepayload", method = RequestMethod.GET)
     public SimplePayload byParameter(
             @RequestParam(
                     value = "payload",
