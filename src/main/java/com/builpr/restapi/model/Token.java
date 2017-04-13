4d80adb00c259ba4303a610f78a4b43bf2af1c93
fwuen <git@fwuen.de>
2017-04-13 14:04:05 +0200

    TODOs für Token.java hinzugefügt

--- src/main/java/com/builpr/restapi/model/Token.java
+++ src/main/java/com/builpr/restapi/model/Token.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 
 import java.util.UUID;
 
+//TODO: Unique-Constraint in Datenbank anlegen
+//TODO: Sollte ein Token doppelt vorkommen, muss ein Fehler von der Datenbank zurückgegeben werden, der eine Neugenerierung des Tokens auslöst
+
 public class Token {
     @Getter
     private String token;
