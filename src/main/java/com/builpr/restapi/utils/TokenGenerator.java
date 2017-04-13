f40e42fda7b182424a0d69354ffb31d0d215a3fd
Dominic Fuchs <syntarex@gmail.com>
2017-04-13 14:30:28 +0200

    Refactoring.
    TESTS FAILING!!

--- src/main/java/com/builpr/restapi/utils/TokenGenerator.java
+++ src/main/java/com/builpr/restapi/utils/TokenGenerator.java
@@ -3,7 +3,6 @@ package com.builpr.restapi.utils;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Verify;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.UUID;
 
@@ -11,7 +10,8 @@ import java.util.UUID;
 //TODO: Sollte ein Token doppelt vorkommen, muss ein Fehler von der Datenbank zurückgegeben werden, der eine Neugenerierung des Tokens auslöst
 public class TokenGenerator {
 
-    @Getter private int tokenSize = 128;
+    @Getter
+    private int tokenSize = 128;
     public void setTokenSize(int tokenSize) {
         Preconditions.checkArgument(tokenSize > 0);
 
