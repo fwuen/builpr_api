83d478ccfff069bb9c0f1f3f472fc7216ff87264
Dominic Fuchs <syntarex@gmail.com>
2017-04-13 14:08:41 +0200

    Privaten Konstruktor hinzugefügt.

--- src/main/java/com/builpr/restapi/utils/TokenGenerator.java
+++ src/main/java/com/builpr/restapi/utils/TokenGenerator.java
@@ -2,7 +2,11 @@ package com.builpr.restapi.utils;
 
 import java.util.UUID;
 
+//TODO: Unique-Constraint in Datenbank anlegen
+//TODO: Sollte ein Token doppelt vorkommen, muss ein Fehler von der Datenbank zurückgegeben werden, der eine Neugenerierung des Tokens auslöst
 public class TokenGenerator {
+
+    private TokenGenerator() { }
 
     public static String generate() {
         return UUID.randomUUID().toString();
