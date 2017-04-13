2a55f8f3cd540195f06d31feb05b28f71bdc580c
Dominic Fuchs <syntarex@gmail.com>
2017-04-13 14:28:56 +0200

    TokenGenerator erweitert sodass Token-Länge variabel ist. Tests erweitert.
    TESTS FAILING!

--- src/main/java/com/builpr/restapi/utils/TokenGenerator.java
+++ src/main/java/com/builpr/restapi/utils/TokenGenerator.java
@@ -1,15 +1,32 @@
 package com.builpr.restapi.utils;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Verify;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
 //TODO: Unique-Constraint in Datenbank anlegen
 //TODO: Sollte ein Token doppelt vorkommen, muss ein Fehler von der Datenbank zurückgegeben werden, der eine Neugenerierung des Tokens auslöst
 public class TokenGenerator {
 
-    private TokenGenerator() { }
+    @Getter private int tokenSize = 128;
+    public void setTokenSize(int tokenSize) {
+        Preconditions.checkArgument(tokenSize > 0);
 
-    public static String generate() {
-        return UUID.randomUUID().toString();
+        this.tokenSize = tokenSize;
+    }
+
+
+
+    public String generate() {
+        String token = UUID.randomUUID().toString();
+
+        Verify.verifyNotNull(token);
+        Verify.verify(token.length() == tokenSize);
+
+        return token;
     }
 
 }
--- src/test/java/com/builpr/restapi/utils/TokenGeneratorTest.java
+++ src/test/java/com/builpr/restapi/utils/TokenGeneratorTest.java
@@ -1,7 +1,10 @@
 package com.builpr.restapi.utils;
 
+import com.google.common.collect.Sets;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Set;
 
 public class TokenGeneratorTest {
 
@@ -10,11 +13,77 @@ public class TokenGeneratorTest {
 
 
     @Test
+    public void create() {
+        new TokenGenerator();
+    }
+
+    @Test
+    public void defaultTokenSizeIsGreaterThenZero() {
+        Assert.assertTrue(new TokenGenerator().getTokenSize() > 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setTokenSizeToZero() {
+        new TokenGenerator().setTokenSize(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setNegativeTokenSize() {
+        new TokenGenerator().setTokenSize(-1);
+    }
+
+    @Test
+    public void setPositiveTokenSize() {
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+        Assert.assertNotNull(tokenGenerator);
+
+        int newSize = 77;
+        tokenGenerator.setTokenSize(77);
+
+        Assert.assertEquals(newSize, tokenGenerator.getTokenSize());
+    }
+
+    @Test
     public void generate() {
-        String token = TokenGenerator.generate();
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+        Assert.assertNotNull(tokenGenerator);
+
+        String token = tokenGenerator.generate();
 
         Assert.assertNotNull(token);
-        Assert.assertEquals(SIZE, token.length());
+        Assert.assertEquals(tokenGenerator.getTokenSize(), token.length());
+    }
+
+    @Test
+    public void setTokenSizeAndGenerate() {
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+        Assert.assertNotNull(tokenGenerator);
+
+        int newSize = 55;
+        tokenGenerator.setTokenSize(55);
+
+        Assert.assertEquals(newSize, tokenGenerator.getTokenSize());
+
+        String token = tokenGenerator.generate();
+
+        Assert.assertNotNull(token);
+        Assert.assertEquals(newSize, token.length());
+    }
+
+    @Test
+    public void tokenIsUnique() {
+        Set<String> tokens = Sets.newHashSet();
+
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+        Assert.assertNotNull(tokenGenerator);
+
+        for(int i = 0; i < 100; i++) {
+            tokens.add(tokenGenerator.generate());
+        }
     }
 
 }
