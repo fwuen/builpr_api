65f1a6701766182d1f351eddbefd446418361caf
fwuen <git@fwuen.de>
2017-05-08 12:45:32 +0200

    Solr angepasst

--- src/main/java/com/builpr/restapi/configuration/SolrConfiguration.java
+++ src/main/java/com/builpr/restapi/configuration/SolrConfiguration.java
@@ -14,14 +14,14 @@ import javax.annotation.Resource;
 @EnableSolrRepositories(basePackages={"com.acme.solr"}, multicoreSupport = true)
 public class SolrConfiguration {
 
-    //TODO: Tests über Spring/Gradle ausführen (sonst wird Spring nicht ausgeführt)
-    //TODO: Server-Property auslagern
+    // TODO: Tests über Spring/Gradle ausführen (sonst wird Spring nicht ausgeführt)
+    // TODO: Server-Property auslagern
     /*
     @Value("solr.host")
     private String solr_host;
     */
 
-    //TODO: baseURL-Parameter mit private-Attribut solr_host ersetzen
+    // TODO: baseURL-Parameter mit private-Attribut solr_host ersetzen
     @Bean
     public SolrClient solrClient() {
         return new HttpSolrClient("localhost");
