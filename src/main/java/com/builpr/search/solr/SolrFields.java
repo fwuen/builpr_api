341cabf9256a419d1e6b4ca308ee1dcadd15446c
fwuen <git@fwuen.de>
2017-05-11 11:11:04 +0200

    Solr-Erweiterungen

--- src/main/java/com/builpr/search/solr/SolrFields.java
+++ src/main/java/com/builpr/search/solr/SolrFields.java
@@ -10,7 +10,7 @@ public class SolrFields {
     //TODO: alle Tags in einem Solr-Field gespeichert?
     public static final String PRINT_MODEL_TAGS = "tags";
     public static final String PRINT_MODEL_AGE_RESTRICTION = "age_restriction";
-    public static final String PRINT_MODEL_uploader = "uploader";
+    public static final String PRINT_MODEL_UPLOADER = "uploader";
     public static final String PRINT_MODEL_UPLOAD_DATE = "upload_date";
     public static final String PRINT_MODEL_RATING = "rating";
 
