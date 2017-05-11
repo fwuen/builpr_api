acf3f66752d572efafe520766ec075750fc0a09a
fwuen <git@fwuen.de>
2017-05-11 10:30:13 +0200

    Weitere Solr-Fields hinzugefügt

--- src/main/java/com/builpr/search/solr/SolrFields.java
+++ src/main/java/com/builpr/search/solr/SolrFields.java
@@ -1,7 +1,19 @@
 package com.builpr.search.solr;
 
+import java.util.List;
+
 public class SolrFields {
 
-    public static String PRINT_MODEL_ID = "id";
+    public static final String PRINT_MODEL_ID = "id";
+    public static final String PRINT_MODEL_TITLE = "title";
+    public static final String PRINT_MODEL_DESCRIPTION = "description";
+    //TODO: alle Tags in einem Solr-Field gespeichert?
+    public static final String PRINT_MODEL_TAGS = "tags";
+    public static final String PRINT_MODEL_AGE_RESTRICTION = "age_restriction";
+    public static final String PRINT_MODEL_uploader = "uploader";
+    public static final String PRINT_MODEL_UPLOAD_DATE = "upload_date";
+    public static final String PRINT_MODEL_RATING = "rating";
+
+
 
 }
