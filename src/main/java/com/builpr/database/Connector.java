63264a451475b525e769026dd56dcb3db28705b8
Marco <marcogeissler69@gmail.com>
2017-05-15 12:44:15 +0200

    Javadoc gelöscht

--- src/main/java/com/builpr/database/Connector.java
+++ src/main/java/com/builpr/database/Connector.java
@@ -1,18 +1,10 @@
 package com.builpr.database;
 
-/**
- * Klasse, die eine DB-Connection herstellt. Im Standardfall wird das normale DB-Schema genutzt, man kann aber auch
- * ein anderes (z.B. für Tests angeben)
- */
+
 public class Connector {
 
     private static String schemaName;
 
-    /**
-     * Diese Methode stellt eine Standardverbindung zur MySql-Datenbank her
-     *
-     * @return BuilprApplication
-     */
     public static BuilprApplication getConnection() {
         if(schemaName == null) {
             return new BuilprApplicationBuilder().withPassword("builpr123").build();
@@ -22,11 +14,6 @@ public class Connector {
 
     }
 
-    /**
-     * Methode, um den Namen des benutzten Schemas zu ändern
-     *
-     * @param newSchemaName der Name des genutzten Schemas
-     */
     public static void setSchemaName(String newSchemaName) {
         schemaName = newSchemaName;
     }
