7b8eb632f3d4d497229ccc880fff7dbe17d22b83
fwuen <git@fwuen.de>
2017-06-06 15:17:25 +0200

    Datenbankzeug

--- src/main/java/com/builpr/restapi/utils/Connector.java
+++ src/main/java/com/builpr/restapi/utils/Connector.java
@@ -3,28 +3,6 @@ package com.builpr.restapi.utils;
 
 import com.builpr.database.BuilprApplication;
 import com.builpr.database.BuilprApplicationBuilder;
-import com.builpr.database.db.builpr.collection.Collection;
-import com.builpr.database.db.builpr.collection.CollectionManager;
-import com.builpr.database.db.builpr.conversation.Conversation;
-import com.builpr.database.db.builpr.conversation.ConversationManager;
-import com.builpr.database.db.builpr.customtags.CustomTags;
-import com.builpr.database.db.builpr.customtags.CustomTagsManager;
-import com.builpr.database.db.builpr.message.MessageManager;
-import com.builpr.database.db.builpr.printable.Printable;
-import com.builpr.database.db.builpr.printable.PrintableManager;
-import com.builpr.database.db.builpr.printablecategory.PrintableCategory;
-import com.builpr.database.db.builpr.printablecategory.PrintableCategoryManager;
-import com.builpr.database.db.builpr.printablecustomtag.PrintableCustomTag;
-import com.builpr.database.db.builpr.printablecustomtag.PrintableCustomTagManager;
-import com.builpr.database.db.builpr.rating.Rating;
-import com.builpr.database.db.builpr.rating.RatingManager;
-import com.builpr.database.db.builpr.user.User;
-import com.builpr.database.db.builpr.user.UserManager;
-import com.builpr.database.db.builpr.usercategories.UserCategories;
-import com.builpr.database.db.builpr.usercategories.UserCategoriesManager;
-import com.builpr.restapi.utils.help_interfaces.DatabaseObject;
-import com.speedment.runtime.core.manager.Manager;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
 
 public class Connector {
