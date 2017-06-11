e38cb98b7fbb6436c3d297836f9e6822b3707a42
fwuen <git@fwuen.de>
2017-06-11 16:59:56 +0200

    Solr - Tests - Lange Strings (beim Indexieren) ausgelagert in Konstantenklasse

--- src/test/java/com/builpr/search/solr/SolrSearchManagerTest.java
+++ src/test/java/com/builpr/search/solr/SolrSearchManagerTest.java
@@ -114,10 +114,10 @@ public class SolrSearchManagerTest {
         Preconditions.checkNotNull(categories);
         Preconditions.checkNotNull(date);
 
-        Printable p1 = Printable.getBuilder().
+        Printable p5 = Printable.getBuilder().
                 withId(10).
-                withTitle("azreiöjypoOIPAÖOIRHQHWEUPRIALDIFGhahueiprqi").
-                withDescription("FPWQOIQEROQWIEUöfuqpweiurhqweriaödööfejÖHPIQUHEWJRK:BNLKJDXVOUIHERQLEHR").
+                withTitle(SolrTestConstants.p5Title).
+                withDescription(SolrTestConstants.p5Description).
                 withRating(5).
                 withCategories(categories).
                 withUploaderId(1).
@@ -125,8 +125,8 @@ public class SolrSearchManagerTest {
                 withNumberOfDownloads(274422342).
                 build();
 
-        Preconditions.checkNotNull(p1);
-        solrSearchManager.addToIndex(p1);
+        Preconditions.checkNotNull(p5);
+        solrSearchManager.addToIndex(p5);
     }
 
     //TODO Test korrigieren, der testet aktuell nicht wirklich was
@@ -149,26 +149,8 @@ public class SolrSearchManagerTest {
 
         Printable p1 = Printable.getBuilder().
                 withId(1).
-                withTitle("car (mercedes c9 inspired)").
-                withDescription(("This car model is loosely based on a mercedes C9 le man car.\n" +
-                        "\n" +
-                        "The model was split in half as it prints better this way. Unfortunately on my set up it warped quite badly and so it could do with some tweaking.\n" +
-                        "\n" +
-                        "The model is pictured in two halves but can be glued together on an unwarped print.\n" +
-                        "\n" +
-                        "I've uploaded a step. file for anyone who would like to edit the model.\n" +
-                        "\n" +
-                        "UPDATE1- I've added two more files, an stl and stp of 'car thingi whole'. This is the same model which will probably require support material but you won't need to glue the two halves together. Thanks for the suggestion jfpion.\n" +
-                        "\n" +
-                        "Please upload a photo if you print one :)\n" +
-                        "\n" +
-                        "The example pictured was printed at 0.2mm on a rep2 with a 25% infill which is definitely overkill. I also halved the default extruder speed in makerware in the hope of improving surface finish. It took about 4 hours to print. The arctic white plastic is from faberdashery, definitely recommended despite the price.\n" +
-                        "\n" +
-                        "I would also suggest enlarging the stl file before printing, as you can see on my print the wheel arch was too thin so it hasn't been filled in properly.\n" +
-                        "\n" +
-                        "The wheels were super glued in place but I'm sure someone could modify the car to allow for rotating wheels. The aim was to glue the two body halves together, which should be straight forward as long as your model doesn't warp!\n" +
-                        "\n" +
-                        "I positioned a number of ear rafts around the car from whpthomas http://www.thingiverse.com/thing:38272. The aim was to reduce warping although the blue tape I was printing on started to lift off the build plate, I think printing PLA straight onto the build plate would be fine or just using stronger wider tape.\n").toLowerCase()).
+                withTitle(SolrTestConstants.p1Title).
+                withDescription(SolrTestConstants.p1Description).
                 withRating(1).
                 withCategories(categories).
                 withUploaderId(1).
@@ -208,7 +190,7 @@ public class SolrSearchManagerTest {
         categories.add("ww1");
         Printable p2 = Printable.getBuilder().
                 withCategories(categories).
-                withDescription("This is just a simple gotha bomber from world war 1. print up on its nose with supports for best result. enjoy!").
+                withDescription(SolrTestConstants.p2Description).
                 withRating(5).
                 withNumberOfDownloads(30).
                 withId(3).
@@ -220,7 +202,7 @@ public class SolrSearchManagerTest {
         categories.clear();
         Printable p3 = Printable.getBuilder().
                 withCategories(categories).
-                withDescription("Its just a thin hookah / shisha...\nNOT smokable... :D").
+                withDescription(SolrTestConstants.p3Description).
                 withRating(3).
                 withNumberOfDownloads(526).
                 withId(4).
@@ -231,40 +213,13 @@ public class SolrSearchManagerTest {
 
         Printable p4 = Printable.getBuilder().
                 withCategories(categories).
-                withDescription("This is a quick model I threw together making a fidget spinner. I thought I'd change things up a bit and make it easier to hold onto and fun to do cool \"tricks\" on almost like a yoyo. Enjoy and do as you wish.\n" +
-                        "Print Settings\n" +
-                        "\n" +
-                        "Printer:\n" +
-                        "Prusa i3 Mk 2\n" +
-                        "\n" +
-                        "Rafts:\n" +
-                        "No\n" +
-                        "\n" +
-                        "Supports:\n" +
-                        "No\n" +
-                        "\n" +
-                        "Resolution:\n" +
-                        ".2\n" +
-                        "\n" +
-                        "Infill:\n" +
-                        "At least 20%\n" +
-                        "\n" +
-                        "\n" +
-                        "Notes:\n" +
-                        "You will need:\n" +
-                        "\n" +
-                        "4x Bearings (preferably ABEC 7 or better)\n" +
-                        "3D printer (Duh)\n" +
-                        "super glue if you wanna be risky\n" +
-                        "sand paper to get correct resistance on cap\n" +
-                        "\n" +
-                        "Print using PETG or PLA for best fit, and sand down caps pole to match inside of bearing for the correct resistance of your bearing (Many Manufacturers are slightly different)").
+                withDescription(SolrTestConstants.p4Description).
                 withRating(2).
                 withNumberOfDownloads(18340).
                 withId(5).
                 withUploadDate(date).
                 withUploaderId(1).
-                withTitle("Trick Fidget Spinner (Three and Two weight models)").
+                withTitle(SolrTestConstants.p4Title).
                 build();
 
         indexables.add(p1);
---
+++ src/test/java/com/builpr/search/solr/SolrTestConstants.java
@@ -0,0 +1,57 @@
+package com.builpr.search.solr;
+
+public class SolrTestConstants {
+    static String p1Title = "car (mercedes c9 inspired)";
+    static String p1Description = "This car model is loosely based on a mercedes C9 le man car.\n" +
+            "\n" +
+            "The model was split in half as it prints better this way. Unfortunately on my set up it warped quite badly and so it could do with some tweaking.\n" +
+            "\n" +
+            "The model is pictured in two halves but can be glued together on an unwarped print.\n" +
+            "\n" +
+            "I've uploaded a step. file for anyone who would like to edit the model.\n" +
+            "\n" +
+            "UPDATE1- I've added two more files, an stl and stp of 'car thingi whole'. This is the same model which will probably require support material but you won't need to glue the two halves together. Thanks for the suggestion jfpion.\n" +
+            "\n" +
+            "Please upload a photo if you print one :)\n" +
+            "\n" +
+            "The example pictured was printed at 0.2mm on a rep2 with a 25% infill which is definitely overkill. I also halved the default extruder speed in makerware in the hope of improving surface finish. It took about 4 hours to print. The arctic white plastic is from faberdashery, definitely recommended despite the price.\n" +
+            "\n" +
+            "I would also suggest enlarging the stl file before printing, as you can see on my print the wheel arch was too thin so it hasn't been filled in properly.\n" +
+            "\n" +
+            "The wheels were super glued in place but I'm sure someone could modify the car to allow for rotating wheels. The aim was to glue the two body halves together, which should be straight forward as long as your model doesn't warp!\n" +
+            "\n" +
+            "I positioned a number of ear rafts around the car from whpthomas http://www.thingiverse.com/thing:38272. The aim was to reduce warping although the blue tape I was printing on started to lift off the build plate, I think printing PLA straight onto the build plate would be fine or just using stronger wider tape.\n";
+    static String p2Description = "This is just a simple gotha bomber from world war 1. print up on its nose with supports for best result. enjoy!";
+    static String p3Description = "Its just a thin hookah / shisha...\nNOT smokable... :D";
+    static String p4Title = "Trick Fidget Spinner (Three and Two weight models)";
+    static String p4Description = "This is a quick model I threw together making a fidget spinner. I thought I'd change things up a bit and make it easier to hold onto and fun to do cool \"tricks\" on almost like a yoyo. Enjoy and do as you wish.\n" +
+            "Print Settings\n" +
+            "\n" +
+            "Printer:\n" +
+            "Prusa i3 Mk 2\n" +
+            "\n" +
+            "Rafts:\n" +
+            "No\n" +
+            "\n" +
+            "Supports:\n" +
+            "No\n" +
+            "\n" +
+            "Resolution:\n" +
+            ".2\n" +
+            "\n" +
+            "Infill:\n" +
+            "At least 20%\n" +
+            "\n" +
+            "\n" +
+            "Notes:\n" +
+            "You will need:\n" +
+            "\n" +
+            "4x Bearings (preferably ABEC 7 or better)\n" +
+            "3D printer (Duh)\n" +
+            "super glue if you wanna be risky\n" +
+            "sand paper to get correct resistance on cap\n" +
+            "\n" +
+            "Print using PETG or PLA for best fit, and sand down caps pole to match inside of bearing for the correct resistance of your bearing (Many Manufacturers are slightly different)";
+    static String p5Title = "azreiöjypoOIPAÖOIRHQHWEUPRIALDIFGhahueiprqi";
+    static String p5Description = "FPWQOIQEROQWIEUöfuqpweiurhqweriaödööfejÖHPIQUHEWJRK:BNLKJDXVOUIHERQLEHR";
+}
