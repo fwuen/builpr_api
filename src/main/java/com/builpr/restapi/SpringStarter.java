3a1233324941990e63d899ed7c8a35c705bdc066
Dominic Fuchs <syntarex@gmail.com>
2017-06-26 21:50:26 +0200

    WAR Building implementiert 2.

--- src/main/java/com/builpr/restapi/SpringStarter.java
+++ src/main/java/com/builpr/restapi/SpringStarter.java
@@ -2,9 +2,16 @@ package com.builpr.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringStarter {
+public class SpringStarter extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringStarter.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStarter.class, args);
