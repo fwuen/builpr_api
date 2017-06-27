bf90ecfdb226e637ba10cc84a8175cdafbdcc4b1
Dominic Fuchs <syntarex@gmail.com>
2017-06-27 21:16:01 +0200

    SearchController Änderungen.

--- src/main/java/com/builpr/restapi/model/Request/Search/SearchPayload.java
+++ src/main/java/com/builpr/restapi/model/Request/Search/SearchPayload.java
@@ -1,6 +1,7 @@
 package com.builpr.restapi.model.Request.Search;
 
 
+import com.google.common.collect.Lists;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,35 +17,35 @@ public class SearchPayload {
      */
     @Getter
     @Setter
-    private String query;
+    private String query = "";
 
     /**
      * Value of the minimal rating
      */
     @Getter
     @Setter
-    private int minimumRatingFilter;
+    private int minimumRatingFilter = 1;
 
     /**
      * list of categories
      */
     @Getter
     @Setter
-    private List<String> categories;
+    private List<String> categories = Lists.newArrayList();
 
     /**
      * value of the order of the printables
      */
     @Getter
     @Setter
-    private String order;
+    private String order = "desc";
 
     /**
      * value of the way the result will be sorted
      */
     @Getter
     @Setter
-    private String sort;
+    private String sort = "relevance";
 
 
 }
