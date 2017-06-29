9736f1fcefd4cb595543f9b1c7cef09f67978584
headnutmonster <marcogeissler69@gmail.com>
2017-06-29 21:06:33 +0200

    GravatarURL und Username zur RatingPayload hinzgefügt

--- src/main/java/com/builpr/restapi/converter/RatingModelToRatingPayloadConverter.java
+++ src/main/java/com/builpr/restapi/converter/RatingModelToRatingPayloadConverter.java
@@ -1,6 +1,7 @@
 package com.builpr.restapi.converter;
 
 import com.builpr.database.bridge.rating.Rating;
+import com.builpr.database.service.DatabaseUserManager;
 import com.builpr.restapi.model.Response.rating.RatingPayload;
 
 import java.util.ArrayList;
@@ -13,11 +14,17 @@ public class RatingModelToRatingPayloadConverter {
 
 
     public static RatingPayload from(Rating rating) {
+        DatabaseUserManager userManager = new DatabaseUserManager();
+        String ownerName = userManager.getByID(rating.getUserId()).getUsername();
+        String ownerGravatarURL = userManager.getByID(rating.getUserId()).getAvatar().get();
+
         return new RatingPayload()
                 .setOwnerID(rating.getUserId())
                 .setRating(rating.getRating())
                 .setText(rating.getMsg().isPresent() ? rating.getMsg().get() : null)
-                .setTime(rating.getRatingTime().toString());
+                .setTime(rating.getRatingTime().toString())
+                .setOwnerUserName(ownerName)
+                .setOwnerGravatarURL(ownerGravatarURL);
     }
 
     public static List<RatingPayload> from(List<Rating> ratings) {
